44a631d5d820f95d9166e6d188ca61170ff32790
emp320 <rjardine@allstate.com>
2016-07-11 23:23:48 -0500

    added populate renter with save to apartment

--- server/models/renter.js
+++ server/models/renter.js
@@ -10,6 +10,20 @@ const renterSchema = new Schema({
   dateCreated: { type: Date, default: Date.now },
 });
 
+ // Apartment.findById(this.apartment)
+ //   .populate('renters')
+ //   .lean(true)
+ //   .exec(function (err, apartment) {
+ //     if (err) return cb(500, err);
+ //     if (!apartment) return cb(404, {
+  //      message: 'no apartment found for ID ' + this.apartment
+ //     });
+//      apartment.collectedRent += apartment.rent;
+//      apartment.save((errSave) => {
+//      if (errSave) return cb(405,'EXCEPTION: PAY RENT FAILED!');
+//      return cb(200,'SUCCESS: RENT PAID!');
+ //   });
+
 renterSchema.methods.pay = function (cb) {
   Apartment.findById(this.apartment, (err, apt) => {
     if (err) return cb('FAILED');
