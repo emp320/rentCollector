149a1f3e07b9eafb6797e0e5a42f8536d83c7d6e
emp320 <rjardine@allstate.com>
2016-07-12 09:18:42 -0500

    fixed

--- server/models/renter.js
+++ server/models/renter.js
@@ -1,5 +1,4 @@
 import mongoose from 'mongoose';
-import Apartment from '../models/apartment';
 
 const Schema = mongoose.Schema;
 
@@ -9,31 +8,5 @@ const renterSchema = new Schema({
   apartment: { type: mongoose.Schema.ObjectId, ref: 'Apartment' },
   dateCreated: { type: Date, default: Date.now },
 });
-
- // Apartment.findById(this.apartment)
- //   .populate('renters')
- //   .lean(true)
- //   .exec( (err, apartment) => {
- //     if (err) return cb(500, err);
- //     if (!apartment) return cb(404, {
-  //      message: 'no apartment found for ID ' + this.apartment
- //     });
-//      apartment.collectedRent += apartment.rent;
-//      apartment.save((errSave) => {
-//      if (errSave) return cb(405,'EXCEPTION: PAY RENT FAILED!');
-//      return cb(200,'SUCCESS: RENT PAID!');
- //   });
-
-renterSchema.methods.pay = function (cb) {
-  Apartment.findById(this.apartment, (err, apt) => {
-    if (err) return cb('FAILED');
-    const rc = apt.collectedRent + apt.rent;
-    apt.collectedRent = rc;
-    apt.save((errSave) => {
-      if (errSave) return cb('EXCEPTION: PAY RENT FAILED!');
-      return cb('SUCCESS: RENT PAID!');
-    });
-  });
-};
 
 module.exports = mongoose.model('Renter', renterSchema)
